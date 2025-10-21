204d320732960a87eb53103fc9f7baf4776a6ee7
sc-esariupendri <esari.upendri@sitecore.com>
2025-10-21 10:04:26 +0530

    Removed jest config

--- kit-nextjs-article-starter/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
-};
